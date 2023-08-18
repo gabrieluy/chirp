976ccaed4114a2e15df24e3519d4c1ba134ad9fe
Gabriel Nuñez <gabirluy@gmail.com>
2023-08-18 19:26:18 +0200

    feat: add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return     <ClerkProvider><Component {...pageProps} /></ClerkProvider>;
 };
 
 export default api.withTRPC(MyApp);
